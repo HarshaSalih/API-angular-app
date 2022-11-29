51fec071ff3f66ca12c62eb68fe701fe20b7f2c0
HarshaSalih <harshasalih2000@gmail.com>
2022-11-29 20:10:28 +0530

    commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ViewProductComponent } from './view-product/view-product.component';
 import { ViewUserdataComponent } from './view-userdata/view-userdata.component';
 import { ViewTodoComponent } from './view-todo/view-todo.component';
 import { ViewQuotesappComponent } from './view-quotesapp/view-quotesapp.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { ViewQuotesappComponent } from './view-quotesapp/view-quotesapp.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
     
   ],
   providers: [],
