f694e7e125e2caf9c3c2bc2d1971bc021c5f0d74
HarshaSalih <harshasalih2000@gmail.com>
2022-11-29 16:27:44 +0530

    error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ViewPassengerComponent } from './view-passenger/view-passenger.component';
 import { ViewPublicdataComponent } from './view-publicdata/view-publicdata.component';
 import { ViewUserinfoComponent } from './view-userinfo/view-userinfo.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewPassengerComponent,
     ViewPublicdataComponent,
-    ViewUserinfoComponent
+    ViewUserinfoComponent,
+    ViewProductComponent
   ],
   imports: [
     BrowserModule,
